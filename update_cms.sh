2a4d4dd0dc5d029fdffe34d33a7a3c6d49d2372d
Caspar Hsieh <caspar.n.hsieh@gmail.com>
2013-12-05 14:12:13 +0800

    Update cms from git repository.

--- update_cms.sh
+++ update_cms.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH
 
-cd /var/www/9x9/cms/
-svn update
+CMS_HOME=/var/www/9x9/cms
+CMS_GIT=git@bitbucket.org:9x9group/9x9_cms.git
+
+# Check cms directory
+[ ! -d "$CMS_HOME/.git" ] && rm -rf $CMS_HOME && git clone $CMS_GIT $CMS_HOME
+
+# Update cms
+cd $CMS_HOME
+git pull
+
+
 
