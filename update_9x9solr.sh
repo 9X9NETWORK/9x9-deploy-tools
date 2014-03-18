25f1145f83d8637a4c67a31da8dcc28c92b6d51e
Caspar Hsieh <caspar.n.hsieh@gmail.com>
2014-03-18 16:23:50 +0800

    Fix update bug for update_9x9solr.sh

--- update_9x9solr.sh
+++ update_9x9solr.sh
@@ -8,7 +8,7 @@ SOLR_GIT=git@bitbucket.org:9x9group/9x9_solr.git
 
 [ ! -d "$SOLR_HOME/.git" ] && sudo rm -rf $SOLR_HOME && sudo mkdir -p $SOLR_HOME && sudo chown ubuntu:ubuntu $SOLR_HOME && git clone $SOLR_GIT $SOLR_HOME
 
-cd $GIT_HOME
+cd $SOLR_HOME
 git pull
 
 sudo /etc/init.d/jetty restart
