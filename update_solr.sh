266af5e0b0f7049bf920be94f39924ab192f74c8
= <=>
2014-03-18 14:55:15 +0800

    Added update_solr.sh deploy script

---
+++ update_solr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# update_solr.sh: Update solr config
+
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+SOLR_HOME=/usr/share/solr
+SOLR_GIT=git@bitbucket.org:9x9group/9x9_solr.git
+
+[ ! -d "$SOLR_HOME/.git" ] && sudo rm -rf $SOLR_HOME && sudo mkdir -p $SOLR_HOME && sudo chown ubuntu:ubuntu $SOLR_HOME && git clone $SOLR_GIT $SOLR_HOME
+
+cd $GIT_HOME
+git pull
+
+sudo /etc/init.d/jetty restart
+
